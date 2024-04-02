30e0d5a59f1774d5c35fbfb77400069858090fcc
903653594@qq.com <49876144+UncAnnyZ@users.noreply.github.com>
2024-04-02 23:19:06 +0800

    feat: 新增发布

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import "./index.css";
 
 
 const App = (props) => {
-  return <div style={{ color: "red" }}>hello  world</div>;
+  return <div style={{ color: "red" }}>hello  asdd  world</div>;
 };
 
 WeDyncamic.App = App;
